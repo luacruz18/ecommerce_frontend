371bc99b45550a5eb557153b19bef4ae17b74a44
sncozzi <santiago.cozzi@anima.edu.uy>
2024-06-06 20:53:28 -0300

    carrusel changes

--- src/components/CarruselCards.jsx
+++ src/components/CarruselCards.jsx
@@ -1,52 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { Carousel } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import React, { useState, useEffect } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/themes/splide-default.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
 
 const CarouselWithControls = () => {
-  const [productos, setProductos] = useState([]);
-  const [index, setIndex] = useState(0);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/products')
-      .then(response => response.json())
-      .then(data => setProductos(data))
-      .catch(error => console.error('Error fetching data:', error));
+    fetch("http://localhost:3000/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
-  const chunkArray = (arr, chunkSize) => {
-    const chunkedArray = [];
-    for (let i = 0; i < arr.length; i += chunkSize) {
-      chunkedArray.push(arr.slice(i, i + chunkSize));
-    }
-    return chunkedArray;
-  };
-
-  const chunkedProducts = chunkArray(productos, 5); // Agrupar los productos en bloques de 4
-
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
-
   return (
-    <div id='explore-container'>
-      <Carousel activeIndex={index} onSelect={handleSelect} interval={null} pause={false} indicators={false}>
-        {chunkedProducts.map((chunk, index) => (
-          <Carousel.Item key={index}>
-            <div className="row justify-content-center">
-              {chunk.map((producto, index) => (
-                <div key={index} className="col-xs-6 col-sm-6 col-md-3 col-lg-2">
-                  <div className="card product-card">
-                    <div className="img-wrapper"><img src={producto.img} className="card-img-top" alt={producto.name} /></div>
-                    <div className="card-body">
-                      <h5 className="card-title">{producto.name}</h5>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </Carousel.Item>
-        ))}
-      </Carousel>
+    <div className="">
+      <div>
+        <Splide
+          options={{
+            type: "loop",
+            perPage: 4,
+            focus: "0",
+            pagination: false
+            
+          }}
+        >
+          {products.map((product, index) => (
+            <SplideSlide style="width: 150px" key={index}>
+              <img src={product.image} alt={product.name} />
+              <h3>{product.name}</h3>
+              <p>{product.description}</p>
+            </SplideSlide>
+          ))}
+        </Splide>
+      </div>
     </div>
   );
 };
