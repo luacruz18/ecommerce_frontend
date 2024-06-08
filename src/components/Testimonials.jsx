96e8c32721c5a5683da801c70748a7ff90351888
Micaela Reyes <vivianamicaela.reyes@gmail.com>
2024-06-07 21:33:39 -0300

    Testimonials
    Fondo

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../pages/Testimonials.css";
+
+export default function Testimonials() {
+  return (
+    <div style={{ width: "100%", height: "80vh", position: "relative" }}>
+      <img
+        src="/public/img/Fondotestimonios.jpg"
+        alt="testimonios"
+        style={{ width: "100%", height: "100%", objectFit: "cover" }}
+      />
+      <div className="testimonials-text">
+        <h1>Testimonios</h1>
+        <p></p>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import CarouselWithCards from '../components/CarruselCards';
-import IndividualIntervalsExample from '../components/Carousel';
-import Footer from '../components/Footer';
-
-
-
+import React from "react";
+import Navbar from "../components/Navbar";
+import CarouselWithCards from "../components/CarruselCards";
+import IndividualIntervalsExample from "../components/Carousel";
+import Footer from "../components/Footer";
+import Testimonials from "../components/Testimonials";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <IndividualIntervalsExample />
-      <h3 className='d-flex justify-content-center mt-3'>EXPLORA NUESTROS PRODUCTOS</h3>
+      <h3 className="d-flex justify-content-center mt-3">
+        EXPLORA NUESTROS PRODUCTOS
+      </h3>
       <CarouselWithCards />
-      <h3>hola</h3>
+      <Testimonials />
       <Footer />
-      
     </div>
   );
 };
