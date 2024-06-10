dc2f91b113d176792148b5c9b4c99efbe6ae836e
Luana Cruz <luana.cruz@anima.edu.uy>
2024-06-09 23:07:08 -0300

    updated carousel images fixed paths

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,25 +6,25 @@ const IndividualIntervalsExample = () => {
     <div className= "main-container">
     <Carousel>
       <Carousel.Item className="main-carousel-item" interval={1100}>
-        <img className="img" src={import.meta.env.VITE_IMG_URL + "bundle.jpg"} alt="" />
+        <img className="img" src={import.meta.env.VITE_IMG_URL + "PrimerCarrusel.jpg"} alt="" />
         <Carousel.Caption>
              {/* aca iria los botones y eso */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="main-carousel-item" interval={1100}>
-        <img className="img" src={import.meta.env.VITE_IMG_URL + "bundle.jpg"} alt="" />
+        <img className="img" src={import.meta.env.VITE_IMG_URL + "SegundoCarrusel.jpg"} alt="" />
         <Carousel.Caption>
         {/* aca iria los botones y eso */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="main-carousel-item" interval={1100}>
-        <img className="img" src={import.meta.env.VITE_IMG_URL + "bundle.jpg"} alt="" />
+        <img className="img" src={import.meta.env.VITE_IMG_URL + "TercerCarrusel.jpg"} alt="" />
         <Carousel.Caption>
            {/* aca iria los botones y eso */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="main-carousel-item" interval={1100}>
-        <img className="img" src={import.meta.env.VITE_IMG_URL + "bundle.jpg"} alt="" />
+        <img className="img" src={import.meta.env.VITE_IMG_URL + "CuartaCarrusel.jpg"} alt="" />
         <Carousel.Caption>
         {/* aca iria los botones y eso */}
         </Carousel.Caption>
