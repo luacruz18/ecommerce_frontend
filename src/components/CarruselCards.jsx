32d0a1af010c8c6ffe9e9f8a21d87e13bb3f187b
sncozzi <santiago.cozzi@anima.edu.uy>
2024-06-10 20:46:21 -0300

    Revert "carrusel"
    This reverts commit 2faee0159f247207f7af0ceb95e885752d9fde5f.

--- src/components/CarruselCards.jsx
+++ src/components/CarruselCards.jsx
@@ -14,32 +14,30 @@ const CarouselWithControls = () => {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
-  const options = {
-    type: "loop",
-    perPage: 4,
-    focus: "center",
-    pagination: false,
-    autoplay: true,
-    pauseOnHover: false,
-    resetProgress: false,
-    interval: 3000 // Cambiar el intervalo según sea necesario
-  };
-
   return (
-    <div className="">
-      <Splide
-        options={options}
-      >
-        {products.map((product, index) => (
-          <SplideSlide key={index}>
-            <div>
-              <img src={product.image} alt={product.name} />
-              <h3>{product.name}</h3>
-            </div>
+    <div style={{ width: "100%", height: "80vh", position: "relative" }}>
+    <img
+      src="public\img\fondotestimonios.jpg"
+      alt="testimonios"
+      style={{ width: "100%", height: "100%", objectFit: "cover" }}
+    />
+    <div className="testimonials-text">
+      <h1>EXPLORA NUESTROS PRODUCTOS</h1>
+      <div>
+        <Splide
+          options={{
+            type: "loop",
+            perPage: 3,
+            autoplay: true,
+          }}
+        >
+          <SplideSlide>
+            <h3>Hola</h3>
           </SplideSlide>
-        ))}
-      </Splide>
+        </Splide>
+      </div>
     </div>
+  </div>
   );
 };
 
