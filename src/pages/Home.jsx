dff565bffc89ee06ed7c7482f409a539e8a51223
sncozzi <santiago.cozzi@anima.edu.uy>
2024-06-11 21:25:18 -0300

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import IndividualIntervalsExample from "../components/Carousel";
 import Footer from "../components/Footer";
 import ProductCarousel from "../components/ProductCarousel";
 import Testimonials from "../components/Testimonials";
+import Collage from "../components/Collage";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <IndividualIntervalsExample />
       <h3 className="text-center mt-3">EXPLORA NUESTROS PRODUCTOS</h3>
       <ProductCarousel />
+      <Collage />
       <Testimonials />
       <Footer />
    
