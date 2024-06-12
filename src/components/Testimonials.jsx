534aab4defc255ea370bd318941b0903a4ccb8af
sncozzi <santiago.cozzi@anima.edu.uy>
2024-06-11 21:22:18 -0300

    update testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,7 +5,7 @@ function Testimonials() {
   return (
     <div className='testimonials'>
       <div className='container'>
-        <h3 className='text-center text-light mb-5 pt-5'>TESTIMONIOS</h3>
+        <h3 className='text-light mb-5 pt-5'>TESTIMONIOS</h3>
         <CardGroup className="row">
           <Card className="col-sm-12 col-md-4">
             <Card.Img variant="top" src="holder.js/100px180" />
