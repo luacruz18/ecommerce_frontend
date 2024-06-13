e19f648fe8e7adee15dddc684b41d3505e1fa974
Sofia Salaberry <sofia.salaberry9810@gmail.com>
2024-06-13 19:58:33 -0300

    NavBarChange

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { BsPersonFill, BsCart } from "react-icons/bs";
 import "../App.css";
 
@@ -13,23 +12,23 @@ const CustomNavbar = () => {
           src={"../img/Logo_Web.png"}
           alt="logo"
           className="logo"
-          style={{ maxHeight: "50px" }} 
+          style={{ maxHeight: "50px" }}
         />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" className="menu" />
-      <Navbar.Collapse id="basic-navbar-nav" className="navbar-nav justify-content-end">
+      <Navbar.Collapse
+        id="basic-navbar-nav"
+        className="navbar-nav justify-content-end"
+      >
         <Nav>
           <Nav.Link href="#features">Features</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <NavDropdown title="Menú" id="dropdown-basic" className="dropdown">
-            <NavDropdown.Item href="#action/3.1">Productos</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Otra cosa</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Otra cosa</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Sobre nosotros
-            </NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="#product" className="product-nav">
+            Productos
+          </Nav.Link>
+          <Nav.Link href="#about" className="about-us-nav">
+            Sobre nosotros
+          </Nav.Link>
           <BsCart className="cart-icon" />
           <BsPersonFill className="person-fill" />
         </Nav>
@@ -39,4 +38,3 @@ const CustomNavbar = () => {
 };
 
 export default CustomNavbar;
-
