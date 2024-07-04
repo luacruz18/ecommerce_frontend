df07d018a4d4da0a8831d7ca6ae3d5a239b89505
sncozzi <santiago.cozzi@anima.edu.uy>
2024-07-03 21:28:54 -0300

    Changes

---
+++ src/components/CollageDos.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import "../styles/Collage.css";
+import { fetchProducts } from "../Hooks/api"; 
+
+const CollageDos = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+
+    const loadProducts = async () => {
+      try {
+        const data = await fetchProducts();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    loadProducts();
+  }, []); 
+
+  const getProductImageUrl = (index) => {
+    if (products[index] && products[index].gallery) {
+      return import.meta.env.VITE_IMG_URL + products[index].gallery;
+    }
+    return "";
+  };
+
+  return (
+    <div>
+      <div className="collage-2 pt-5">
+        <div className="row m-0">
+          <div className="col-6">
+            <div className="parent text-light">
+              <div className="div1" style={{ backgroundImage: `url(${getProductImageUrl(0)})` }}>
+                {products[0] && <h3>{products[0].name}</h3>}
+              </div>
+              <div className="div2" style={{ backgroundImage: `url(${getProductImageUrl(1)})` }}>
+              <h3>{products[1] && products[1].name}</h3>
+              </div>
+              <div className="div3" style={{ backgroundImage: `url(${getProductImageUrl(2)})` }}>
+              <h3>{products[2] && products[2].name}</h3>
+              </div>
+              <div className="div4" style={{ backgroundImage: `url(${getProductImageUrl(3)})` }}>
+              <h3>{products[3] && products[3].name}</h3>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="parent-2 text-light">
+              <div className="div5" style={{ backgroundImage: `url(${getProductImageUrl(4)})` }}>
+              <h3>{products[4] && products[4].name}</h3>
+              </div>
+              <div className="div6" style={{ backgroundImage: `url(${getProductImageUrl(5)})` }}>
+              <h3>{products[5] && products[5].name}</h3>
+              </div>
+              <div className="div7" style={{ backgroundImage: `url(${getProductImageUrl(6)})` }}>
+              <h3>{products[6] && products[6].name}</h3>
+              </div>
+              <div className="div8" style={{ backgroundImage: `url(${getProductImageUrl(7)})` }}>
+              <h3>{products[7] && products[7].name}</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CollageDos;
