05b0c5e8753db9d3596f48f6ffb7b4d735c435c4
Sofia9810 <148917271+Sofia9810@users.noreply.github.com>
2024-07-12 17:05:57 -0300

    CambiosIncompletos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,70 +1,40 @@
 import React from "react";
-import "../styles/Footer.css"; 
+import "../styles/Footer.css"; // Asegúrate de que la ruta del archivo CSS sea correcta
 
 const Footer = () => (
-  <footer className="page-footer font-small pt-4 text-light ">
-    <div className=" text-center text-md-left">
-      <div className="row m-0">
-        <div className="col-md-4 mt-md-0 mt-3 d-flex flex-column align-items-center align-md-start">
+  <footer className="footer"> {/* Aplica la clase 'footer' para los estilos generales */}
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4 mt-3">
           <h5 className="text-uppercase">¡Gracias por Visitarnos!</h5>
-          <div className="icons-footer d-flex justify-content-center">
-            <i className="bi bi-facebook icon "> </i>
-            <i className="bi bi-twitter-x icon "></i>
-            <i className="bi bi-youtube icon "></i>
-            <i className="bi bi-instagram icon "></i>
-            <i className="bi bi-tiktok icon"></i>
-          </div>
-        </div>
-
-        <hr className="clearfix w-100 d-md-none pb-0" />
-
-        <div className="col-md-4 mb-md-0 mb-3">
-          <ul className="list-unstyled">
-            <li>
-              <a href="#!" className="footer-link">
-                Pagina principal
-              </a>
-            </li>
-            <li>
-              <a href="#!" className="footer-link">
-                Novedades
-              </a>
-            </li>
-            <li>
-              <a href="#!" className="footer-link">
-                Contacto
-              </a>
-            </li>
-            <li>
-              <a href="#!" className="footer-link">
-                Ayuda
-              </a>
-            </li>
+          <ul className="icons-footer">
+            <li><i className="bi bi-facebook icon"></i></li>
+            <li><i className="bi bi-twitter-x icon"></i></li>
+            <li><i className="bi bi-youtube icon"></i></li>
+            <li><i className="bi bi-instagram icon"></i></li>
+            <li><i className="bi bi-tiktok icon"></i></li>
           </ul>
         </div>
 
-        <div className="col-md-4 mb-md-0 mb-3">
-          <ul className="list-unstyled">
-            <li>
-              <a href="#!" className="footer-link">
-                Política de Privacidad
-              </a>
-            </li>
-            <li>
-              <a href="#!" className="footer-link">
-                Términos y Condiciones
-              </a>
-            </li>
-            <li>
-              <a href="#!" className="footer-link">
-                Sobre Nosotros
-              </a>
-            </li>
+        <div className="col-md-4 mb-3">
+          <ul className="nav-footer">
+            <li><a href="#!" className="footer-link">Pagina principal</a></li>
+            <li><a href="#!" className="footer-link">Novedades</a></li>
+            <li><a href="#!" className="footer-link">Contacto</a></li>
+            <li><a href="#!" className="footer-link">Ayuda</a></li>
+          </ul>
+        </div>
+
+        <div className="col-md-4 mb-3">
+          <ul className="second-nav-footer">
+            <li><a href="#!" className="footer-link">Política de Privacidad</a></li>
+            <li><a href="#!" className="footer-link">Términos y Condiciones</a></li>
+            <li><a href="#!" className="footer-link">Sobre Nosotros</a></li>
           </ul>
         </div>
       </div>
     </div>
-    <div className="footer-copyright text-center py-3">
+    <div className="text-center text-muted">
       <p className="mb-0">© 2024 Copyright ZonaGamer</p>
     </div>
   </footer>
