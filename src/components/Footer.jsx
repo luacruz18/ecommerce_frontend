4abec6915bb4bd0fad099a5effc57d381d5303c3
Sofia Salaberry <sofia.salaberry9810@gmail.com>
2024-07-12 18:15:27 -0300

    Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,35 +1,73 @@
 import React from "react";
-import "../styles/Footer.css"; // Asegúrate de que la ruta del archivo CSS sea correcta
+import "../styles/Footer.css";
 
 const Footer = () => (
-  <footer className="footer"> {/* Aplica la clase 'footer' para los estilos generales */}
+  <footer className="footer">
     <div className="container">
       <div className="row">
         <div className="col-md-4 mt-3">
           <h5 className="text-uppercase">¡Gracias por Visitarnos!</h5>
           <ul className="icons-footer">
-            <li><i className="bi bi-facebook icon"></i></li>
-            <li><i className="bi bi-twitter-x icon"></i></li>
-            <li><i className="bi bi-youtube icon"></i></li>
-            <li><i className="bi bi-instagram icon"></i></li>
-            <li><i className="bi bi-tiktok icon"></i></li>
+            <li>
+              <i className="bi bi-facebook icon"></i>
+            </li>
+            <li>
+              <i className="bi bi-twitter-x icon"></i>
+            </li>
+            <li>
+              <i className="bi bi-youtube icon"></i>
+            </li>
+            <li>
+              <i className="bi bi-instagram icon"></i>
+            </li>
+            <li>
+              <i className="bi bi-tiktok icon"></i>
+            </li>
           </ul>
         </div>
 
         <div className="col-md-4 mb-3">
           <ul className="nav-footer">
-            <li><a href="#!" className="footer-link">Pagina principal</a></li>
-            <li><a href="#!" className="footer-link">Novedades</a></li>
-            <li><a href="#!" className="footer-link">Contacto</a></li>
-            <li><a href="#!" className="footer-link">Ayuda</a></li>
+            <li>
+              <a href="#!" className="footer-link">
+                Página principal
+              </a>
+            </li>
+            <li>
+              <a href="#!" className="footer-link">
+                Novedades
+              </a>
+            </li>
+            <li>
+              <a href="#!" className="footer-link">
+                Contacto
+              </a>
+            </li>
+            <li>
+              <a href="#!" className="footer-link">
+                Ayuda
+              </a>
+            </li>
           </ul>
         </div>
 
         <div className="col-md-4 mb-3">
           <ul className="second-nav-footer">
-            <li><a href="#!" className="footer-link">Política de Privacidad</a></li>
-            <li><a href="#!" className="footer-link">Términos y Condiciones</a></li>
-            <li><a href="#!" className="footer-link">Sobre Nosotros</a></li>
+            <li>
+              <a href="#!" className="footer-link">
+                Política de Privacidad
+              </a>
+            </li>
+            <li>
+              <a href="#!" className="footer-link">
+                Términos y Condiciones
+              </a>
+            </li>
+            <li>
+              <a href="#!" className="footer-link">
+                Sobre Nosotros
+              </a>
+            </li>
           </ul>
         </div>
       </div>
